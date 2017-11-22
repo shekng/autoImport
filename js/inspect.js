1a449eb99f1ba00ce3406448d580c5b5b8e8f740
Shek H. Ng <shek.ng@mopro.com>
2017-11-22 15:50:57 -0800

    commit

--- js/inspect.js
+++ js/inspect.js
@@ -6,7 +6,8 @@ window.inspector = {
         
     },  
     run: function(){
-        $("body").find("*").contents().each(function() {             
+        /*
+        $("body").find("*").not("iframe").contents().each(function() {             
             var $this = $(this);
             if (this.nodeType === 3 && this.textContent.trim().length) {
                 $this.parent().css({border: "3px green solid"});
@@ -17,6 +18,100 @@ window.inspector = {
                 }
             }
         });    
+        */
+        
+        var arr = [], arrY = []; arrXLineCnt = [], arrYLineCnt = [];
+        this.goDown(arr, 0, 3, $("body"));
+        arr.shift(); // remove body;
+        
+        var oRect = arr[0].get(0).getBoundingClientRect();
+        //var iBlockS = oRect.top, iBlockE = oRect.bottom;
+        var iBlockS = 0, iBlockE = 0;
+        //iW = this.findContainerWidth(arr);
+        $.each(arr, function(i,v){
+            //console.log(v[0]);
+            oRect = v[0].getBoundingClientRect();
+            
+            // within
+            if (iBlockS <= oRect.top && iBlockE >= oRect.bottom) {
+                v.css({border: "3px orange dashed"});
+            }
+            
+            if (oRect.top >= iBlockE) {
+                v.css({border: "3px green dashed"});
+                //$("<div></div>").css({position: "absolute", "z-index": 9999, "pointer-events": "none", top: oRect.top, left: "0px", width: "100%", height: oRect.height-5, border: "3px green dashed"}).appendTo("body");                    
+                iBlockS = oRect.top;
+                iBlockE = oRect.bottom;
+            }
+                            
+            //$("<div></div>").css({opacity:.5, position: "absolute", "z-index": 9999, "pointer-events": "none", top: oRect.top, left: "0px", width: "100%", height: "1px", border: "1px orange solid"}).appendTo("body");
+            //$("<div></div>").css({opacity:.5, position: "absolute", "z-index": 9999, "pointer-events": "none", top: oRect.bottom, left: "0px", width: "100%", height: "1px", border: "1px red dashed"}).appendTo("body");
+            //$("<div></div>").css({position: "absolute", top: oRect.top, left: oRect.left, width: "1px", height: oRect.height, border: "1px red solid"}).appendTo("body");
+            //$("<div></div>").css({position: "absolute", top: oRect.top, left: oRect.right, width: "1px", height: oRect.height, border: "1px red dashed"}).appendTo("body");
+            //$("<div></div>").css({position: "absolute", top: 0, left: oRect.left, width: "1px", height: iBodyH, border: "1px red solid"}).appendTo("body");
+            //$("<div></div>").css({position: "absolute", top: 0, left: oRect.right, width: "1px", height: iBodyH, border: "1px red dashed"}).appendTo("body");                        
+        }); 
+        
+        /*
+        $.each(arr, function(i,v){
+            //console.log(v[0]);
+            oRect = v[0].getBoundingClientRect();
+            var iX = Math.round(oRect.x);
+            var iY = Math.round(oRect.y);
+            var iBodyH = $(document).height();
+            
+            if (iBlockS 
+            //$("<div></div>").css({position: "absolute", top: iY, left: "0px", width: "100%", height: "1px", border: "1px green solid"}).appendTo("body");
+            //$("<div></div>").css({position: "absolute", top: oRect.bottom, left: "0px", width: "100%", height: "1px", border: "1px green dashed"}).appendTo("body");
+            $("<div></div>").css({position: "absolute", top: oRect.top, left: oRect.left, width: "1px", height: oRect.height, border: "1px red solid"}).appendTo("body");
+            $("<div></div>").css({position: "absolute", top: oRect.top, left: oRect.right, width: "1px", height: oRect.height, border: "1px red dashed"}).appendTo("body");
+            //$("<div></div>").css({position: "absolute", top: 0, left: oRect.left, width: "1px", height: iBodyH, border: "1px red solid"}).appendTo("body");
+            //$("<div></div>").css({position: "absolute", top: 0, left: oRect.right, width: "1px", height: iBodyH, border: "1px red dashed"}).appendTo("body");                        
+        }); 
+        */
+    },
+    goDown: function(arr, iLev, iMax, $ele) {
+        var me = this, arrChildren = $ele.children();  
+
+        // only one child and height is greater than half of the document        
+        if (arrChildren.length === 1 && arrChildren[0].getBoundingClientRect().height > $(document).height()/2) {
+            me.goDown(arr, iLev, iMax, arrChildren.eq(0));
+        }
+        
+        if ($ele[0].nodeType === 1 && $ele[0].tagName !== "SCRIPT") {            
+            //$ele.attr({"lev":iLev, "mark": arr.length});           
+            if ($ele[0].getBoundingClientRect().height < $(document).height()/2) {
+                arr.push($ele);        
+            }
+            
+            if (arrChildren.length && iLev < iMax) {            
+                $.each(arrChildren, function(iIdx, domEle) {
+                    me.goDown(arr, iLev+1, iMax, arrChildren.eq(iIdx));
+                });
+            }
+        }
+    },
+    findContainerWidth: function(arr) {
+        obj = {};
+        $.each(arr, function() {
+            var iW = this.outerWidth();            
+            if (obj["w"+iW]) {
+                obj["w"+iW]++;
+            }
+            else {
+                obj["w"+iW] = 1;
+            }
+        });
+        
+        iMax = 0, sMaxWidth = ""
+        $.each(obj, function(i,v) {            
+            if (v > iMax) {
+                iMax = v;
+                sMaxWidth = i;
+            }
+        });
+                
+        return (sMaxWidth.length) ? parseInt(sMaxWidth.substr(1)) : 0;
     },
     run3: function() {
         var me = this;
